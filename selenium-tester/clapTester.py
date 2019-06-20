d8d71e95107e2ed3408156dba6ed9b89279c967f
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-06-20 18:47:25 +0100

    find clap button

--- selenium-tester/clapTester.py
+++ selenium-tester/clapTester.py
@@ -3,6 +3,6 @@ import os
 from selenium import webdriver
 browser = webdriver.Chrome(executable_path=r"/Users/angelogiacco/Documents/GitHub/mediumBooster/selenium-tester/chromedriver")
 browser.get('https://medium.com/ahmed-t-ali/lets-build-a-github-pro-bot-5e155cec395f')
-clap_button = browser.find_elements_by_xpath('//*[@id="_obv.shell._surface_1561035526164"]/div/main/article/footer/div[1]/div[3]/div/div[1]/div/div/button')[0]
+clap_button = browser.find_element_by_class_name('clapButton')
 for i in range(10):
     clap_button.click()
